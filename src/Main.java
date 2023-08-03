45fbb3c8a51bc51d772b5989244d30700acc484a
vincenzodivita <divitavincenzo@icloud.com>
2023-08-03 23:48:42 +0200

    Add other var

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        // Personal info
         String name = "Vincenzo";
         String lastname = "Di Vita";
-        System.out.println(name + " " + lastname);
+        String country = "Italy";
+        int age = 28;
+        String profession = "Web Developer";
+
+        // Print info
+        System.out.println("Hello, I am " + name + " " + lastname + ".\n");
+        System.out.println("I am a " + age + " y.o. " + profession + " from " + country + ",");
+        System.out.println();
+
     }
 }
